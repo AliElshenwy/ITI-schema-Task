4094d8fcdeddedf91fe541184dffa5146e18c171
AliElshenwy <elshenwy.01090555@gmail.com>
2024-08-16 06:26:34 +0300

    Add project files.

---
+++ ITI schema/Context/iTIDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ITI_schema.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_schema.Context
+{
+    internal class iTIDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-722BKMG; Database = ITI ; Trusted_Connection = True ;Encrypt = False ");
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+       
+       
+
+    }
+}
---
+++ ITI schema/Program.cs
@@ -0,0 +1,19 @@
+﻿using ITI_schema.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITI_schema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new object 
+            iTIDbContext itiDbContext = new iTIDbContext();
+                
+
+            itiDbContext.Database.Migrate();
+
+            itiDbContext.Students.Where(S => S.ID == 0);
+        }
+    }
+}
